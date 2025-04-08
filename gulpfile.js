ed544bf30fa970b9533211162dee06c1a8baffd5
Nicholas Brown <nb@brownsfromsa.com>
2025-04-08 12:54:17 -0400

    Added validate HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, series, watch } = require('gulp'),
-    htmlCompressor = require('gulp-htmlmin');
+    htmlCompressor = require('gulp-htmlmin'),
+    htmlValidator = require('gulp-html');
 
 let compressHTML = () => {
     return src(`*.html`)
@@ -7,4 +8,10 @@ let compressHTML = () => {
         .pipe(dest(`prod/`));
 };
 
+let validateHTML = () => {
+    return src([`*.html`])
+        .pipe(htmlValidator(undefined));
+};
+
 exports.compressHTML = compressHTML;
+exports.validateHTML = validateHTML;
